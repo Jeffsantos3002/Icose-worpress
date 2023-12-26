baa284b7578be79255be28542369d46428e3877f
Jeffsantos3002 <jefersondeoliveira062@gmail.com>
2023-12-26 08:12:11 -0300

    (feat) config custom post membros :sparkles:

--- serve/wp-content/themes/icose/custom_post_type/membros.php
+++ serve/wp-content/themes/icose/custom_post_type/membros.php
@@ -44,6 +44,7 @@ function custom_post_type_membros() {
         'menu_icon'             => 'dashicons-groups', // Ícone do menu (pode ser alterado)
         'show_in_admin_bar'     => true,
         'show_in_nav_menus'     => true,
+        'show_in_rest'          => true,
         'can_export'            => true,
         'has_archive'           => true,
         'exclude_from_search'   => false,
